5e789028af4176e42d6ff2d60293944ffbbe7293
Hajime Nakagami <nakagami@gmail.com>
2020-10-18 20:16:07 +0900

    update Copyright year

--- test_minicql.py
+++ test_minicql.py
@@ -2,7 +2,7 @@
 ###############################################################################
 # MIT License
 #
-# Copyright (c) 2017 Hajime Nakagami
+# Copyright (c) 2017,2020 Hajime Nakagami
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
